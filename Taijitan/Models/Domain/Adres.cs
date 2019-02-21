2b51f01f6b6510cd280a637d2239b612215aa96c
Bono Opalfvens <bono.opalfvens@student.hogent.be>
2019-02-21 18:56:49 +0100

    Edit setters (NullOrWhiteSpace)

--- Taijitan/Models/Domain/Adres.cs
+++ Taijitan/Models/Domain/Adres.cs
@@ -6,11 +6,80 @@ namespace Taijitan.Models.Domain
     public class Adres
     {
         #region Properties
-        public String Land { get; set; }
-        public String Postcode { get; set; }
-        public String Stad { get; set; }
-        public String Straat { get; set; }
-        public String Nummer { get; set; }
+        public String Land {
+            get { return Land; }
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Land mag geen lege waarde bevatten.");
+                }
+                else
+                {
+                    Land = value;
+                }
+            }
+        }
+        public String Postcode
+        {
+            get { return Postcode; }
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Postcode mag geen lege waarde bevatten.");
+                }
+                else
+                {
+                    Postcode = value;
+                }
+            }
+        }
+        public String Stad
+        {
+            get { return Stad; }
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Stad mag geen lege waarde bevatten.");
+                }
+                else
+                {
+                    Stad = value;
+                }
+            }
+        }
+        public String Straat
+        {
+            get { return Straat; }
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Straat mag geen lege waarde bevatten.");
+                }
+                else
+                {
+                    Straat = value;
+                }
+            }
+        }
+        public String Nummer
+        {
+            get { return Nummer; }
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Nummer mag geen lege waarde bevatten.");
+                }
+                else
+                {
+                    Nummer = value;
+                }
+            }
+        }
         #endregion
 
         #region Constructor
