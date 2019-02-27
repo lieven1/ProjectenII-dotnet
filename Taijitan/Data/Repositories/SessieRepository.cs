2aa607e4c5b4840afae19cb1b177b7ea509930dd
Steve Kusters <steve.kusters@hotmail.com>
2019-02-27 15:19:31 +0100

    Repository connectie met DB is mogelijk

--- Taijitan/Data/Repositories/SessieRepository.cs
+++ Taijitan/Data/Repositories/SessieRepository.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Data.SqlClient;
 using Taijitan.Models.Domain;
 
 namespace Taijitan.Data.Repositories
 {
     public class SessieRepository : ISessieRepository
     {
+        private string connectionString = @"Data Source=LAPTOP-3NH1NFIB; Initial Catalog=TaijitanYoshinRyu; User ID=TaijitanAdmin; Password=a";
+        private SqlConnection connection;
+        private SqlDataAdapter adapter;
+
+        public SessieRepository()
+        {
+            connection = new SqlConnection(connectionString);
+            adapter = new SqlDataAdapter();
+        }
+
+
     }
 }
