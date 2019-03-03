e352df9e55dca0c3a912b8f223a97b9a292ff602
Bono Opalfvens <bono.opalfvens@student.hogent.be>
2019-03-03 18:50:46 +0100

    [Refactor] Aanpassing view Edit Gebruiker

--- Taijitan/Models/GebruikerViewModels/GebruikerEditViewModel.cs
+++ Taijitan/Models/GebruikerViewModels/GebruikerEditViewModel.cs
@@ -4,32 +4,35 @@ using Taijitan.Models.Domain;
 
 namespace Taijitan.Models.GebruikerViewModels {
     public class GebruikerEditViewModel {
-        
         [Required(ErrorMessage = "Naam is verplicht.")]
         public String Naam { get; set; }
         [Required(ErrorMessage = "Voornaam is verplicht.")]
         public String Voornaam { get; set; }
-        [Required]
-        [DataType(DataType.Date)]
-        public DateTime Geboortedatum { get; set; }
-        [Required]
-        [Phone]
+        [Phone(ErrorMessage = "Ongeldige waarde voor telefoonnummer.")]
         [RegularExpression(@"((?:\+|00)[17](?: |\-)?|(?:\+|00)[1-9]\d{0,2}(?: |\-)?|(?:\+|00)1\-\d{3}(?: |\-)?)?(0\d|\([0-9]{3}\)|[1-9]{0,3})(?:((?: |\-)[0-9]{2}){4}|((?:[0-9]{2}){4})|((?: |\-)[0-9]{3}(?: |\-)[0-9]{4})|([0-9]{7}))", ErrorMessage = "Ongeldige waarde voor telefoonnummer.")]
         public String TelefoonNummer { get; set; }
-        [Required]
-        [EmailAddress]
+        [Required(ErrorMessage = "Gsmnummer is verplicht.")]
+        [Phone(ErrorMessage = "Ongeldige waarde voor gsmnummer.")]
+        [RegularExpression(@"((?:\+|00)[17](?: |\-)?|(?:\+|00)[1-9]\d{0,2}(?: |\-)?|(?:\+|00)1\-\d{3}(?: |\-)?)?(0\d|\([0-9]{3}\)|[1-9]{0,3})(?:((?: |\-)[0-9]{2}){4}|((?:[0-9]{2}){4})|((?: |\-)[0-9]{3}(?: |\-)[0-9]{4})|([0-9]{7}))", ErrorMessage = "Ongeldige waarde voor gsmnummer.")]
+        public String Gsmnummer { get; set; }
+        [Required(ErrorMessage = "Email is verplicht.")]
+        [EmailAddress(ErrorMessage = "Ongeldige waarde voor e-mailadres.")]
         [RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$", ErrorMessage = "Ongeldige waarde voor e-mailadres.")]
         public String Email { get; set; }
-        [Required]
+        [Display(Name = "Email ouders")]
+        [EmailAddress(ErrorMessage = "Ongeldige waarde voor e-mailadres.")]
+        [RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$", ErrorMessage = "Ongeldige waarde voor e-mailadres.")]
+        public String EmailOuders { get; set; }
+        [Required(ErrorMessage = "Land is verplicht.")]
         public String Land { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Postcode is verplicht.")]
         [RegularExpression(@"^\d{4}$", ErrorMessage = "Ongeldige waarde voor postcode.")]
         public String Postcode { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Stad is verplicht.")]
         public String Stad { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Straat is verplicht.")]
         public String Straat { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Nummer is verplicht.")]
         public String Nummer { get; set; }
 
         public GebruikerEditViewModel()
@@ -41,9 +44,10 @@ namespace Taijitan.Models.GebruikerViewModels {
         {
             this.Naam = gebruiker.Naam;
             this.Voornaam = gebruiker.Voornaam;
-            this.Geboortedatum = gebruiker.Geboortedatum;
             this.TelefoonNummer = gebruiker.Telefoonnummer;
+            this.Gsmnummer = gebruiker.Gsmnummer;
             this.Email = gebruiker.Email;
+            this.EmailOuders = gebruiker.EmailOuders;
             this.Land = gebruiker.Adres.Land;
             this.Postcode = gebruiker.Adres.Postcode;
             this.Stad = gebruiker.Adres.Stad;
