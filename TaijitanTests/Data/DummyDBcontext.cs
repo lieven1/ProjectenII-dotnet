c741bbc09787e723653cd2e5bf77f2d28d7ac33a
Steve Kusters <steve.kusters@hotmail.com>
2019-03-05 22:02:42 +0100

    Lesmoment tests - zeflde bug als registratiecontrollertest

--- TaijitanTests/Data/DummyDBcontext.cs
+++ TaijitanTests/Data/DummyDBcontext.cs
@@ -11,11 +11,15 @@ namespace TaijitanTests.Data
         public List<Gebruiker> GeenGebruikers { get { return new List<Gebruiker>(); } }
         public List<Gebruiker> Lijst1Gebruiker { get; }
         public List<GebruikerVM> Lijst1GebruikerVM { get; }
+        public Gebruiker GebruikerNietInLijst { get; }
+        public Gebruiker GebruikerInLijst { get; }
 
 
         public DummyDBcontext()
         {
-            Gebruiker gebruiker1 = new Gebruiker("username1", "256321-12569", DateTime.Now, "name1", "first name1", Taijitan.Models.Domain.Enums.Geslacht.Man, DateTime.Now, "Gent", "00712345678", "0236587496", "something@some.th", "somet@som.th", new Adres("België", "9000", "Gent", "Voskenslaan", "1"), 100, new Gradatie(1, "", "name"), Taijitan.Models.Domain.Enums.TypeGebruiker.Lid);
+            Gebruiker gebruiker1 = new Gebruiker("username1", "256321-12569", DateTime.Now, "name1", "first name1", Taijitan.Models.Domain.Enums.Geslacht.Man, new DateTime(1990, 1, 1), "Gent", "00712345678", "0236587496", "something@some.th", "somet@som.th", new Adres("België", "9000", "Gent", "Voskenslaan", "1"), 100, new Gradatie(1, "", "name"), Taijitan.Models.Domain.Enums.TypeGebruiker.Lid);
+            Gebruiker GebruikerNietInLijst = new Gebruiker("username2", "256321-00000", DateTime.Now, "name2", "first name2", Taijitan.Models.Domain.Enums.Geslacht.Vrouw, new DateTime(1990, 1, 1), "Gent", "00712345678", "0236587496", "something@some.th", "somet@som.th", new Adres("België", "9000", "Gent", "Voskenslaan", "1"), 100, new Gradatie(1, "", "name"), Taijitan.Models.Domain.Enums.TypeGebruiker.Lid);
+            GebruikerInLijst = gebruiker1;
 
             Lijst1Gebruiker = new List<Gebruiker>();
             Lijst1Gebruiker.Add(gebruiker1);
---
+++ TaijitanTests/Models/Domain/LesmomentTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Taijitan.Models.Domain;
+using TaijitanTests.Data;
+using Xunit;
+
+namespace TaijitanTests.Models.Domain
+{
+
+
+    public class LesmomentTest
+    {
+
+        private readonly DummyDBcontext _context;
+        private readonly Lesmoment lesmoment;
+
+        public LesmomentTest()
+        {
+            _context = new DummyDBcontext();
+            lesmoment = new Lesmoment(new DateTime(2020, 1, 1), new DateTime(2021, 1, 1), _context.Lijst1Gebruiker);
+        }
+
+        #region constructorTests
+
+        [Fact]
+        public void MaakLesmomentAan_NullCheckGebruikers_ExceptionRaised()
+        {
+            Assert.Throws<Exception>(() => new Lesmoment(new DateTime(2019, 1, 1), new DateTime(2020, 1, 1), null));
+        }
+
+        [Fact]
+        public void MaakLesmomentAan_LaterStartDanEindTijd_ExceptionRaised()
+        {
+            Assert.Throws<Exception>(() => new Lesmoment(new DateTime(2020, 1, 1), new DateTime(2019, 1, 1), _context.Lijst1Gebruiker));
+        }
+
+        [Fact]
+        public void MaakLesmomentAan_AllesCorrect_Valid()
+        {
+            var result = new Lesmoment(new DateTime(2020, 1, 1), new DateTime(2021, 1, 1), _context.Lijst1Gebruiker);
+            Assert.IsType<Lesmoment>(result);
+        }
+
+        [Fact]
+        public void MaakLesmomentAan_StartijdInVerleden_ExceptionRaised()
+        {
+            Assert.Throws<Exception>(() => new Lesmoment(new DateTime(1900, 1, 1), new DateTime(1900, 2, 2), _context.Lijst1Gebruiker));
+        }
+        #endregion
+
+        #region registreerLidTests
+        /*
+        [Fact]
+        public void RegistreerLid_OnbestaandeGebruiker_valid()
+        {
+            lesmoment.RegistreerLid(_context.GebruikerNietInLijst);
+            Assert.Contains(_context.GebruikerNietInLijst, lesmoment.geefAanwezigeLeden());
+        }
+        */
+
+        /*
+        [Fact]
+        public void RegistreerLid_IngeschrevenGebruiker_Valid()
+        {
+            lesmoment.RegistreerLid(_context.GebruikerInLijst);
+            Assert.Contains(_context.GebruikerInLijst, lesmoment.geefAanwezigeLeden());
+        }
+        */
+        #endregion
+    }
+}
