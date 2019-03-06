eeba9da9f3e9f32bfe791351fcd5bcae803a7e84
Steve Kusters <steve.kusters@hotmail.com>
2019-03-06 12:22:57 +0100

    Tests uit comments gehaald: werken door eerdere bugfix

--- TaijitanTests/Models/Domain/LesmomentTest.cs
+++ TaijitanTests/Models/Domain/LesmomentTest.cs
@@ -24,47 +24,45 @@ namespace TaijitanTests.Models.Domain
         [Fact]
         public void MaakLesmomentAan_NullCheckGebruikers_ExceptionRaised()
         {
-            Assert.Throws<Exception>(() => new Lesmoment(new DateTime(2019, 1, 1), new DateTime(2020, 1, 1), null));
+            Assert.Throws<ArgumentException>(() => new Lesmoment(new DateTime(2019, 1, 1), new DateTime(2020, 1, 1), null));
         }
 
         [Fact]
         public void MaakLesmomentAan_LaterStartDanEindTijd_ExceptionRaised()
         {
-            Assert.Throws<Exception>(() => new Lesmoment(new DateTime(2020, 1, 1), new DateTime(2019, 1, 1), _context.Lijst1Gebruiker));
+            Assert.Throws<ArgumentException>(() => new Lesmoment(new DateTime(2020, 1, 1), new DateTime(2019, 1, 1), _context.Lijst1Gebruiker));
         }
 
         [Fact]
         public void MaakLesmomentAan_AllesCorrect_Valid()
         {
-            var result = new Lesmoment(new DateTime(2020, 1, 1), new DateTime(2021, 1, 1), _context.Lijst1Gebruiker);
+            var result = new Lesmoment(new DateTime(2020, 1, 1), new DateTime(2020, 1, 2), _context.Lijst1Gebruiker);
             Assert.IsType<Lesmoment>(result);
         }
 
         [Fact]
         public void MaakLesmomentAan_StartijdInVerleden_ExceptionRaised()
         {
-            Assert.Throws<Exception>(() => new Lesmoment(new DateTime(1900, 1, 1), new DateTime(1900, 2, 2), _context.Lijst1Gebruiker));
+            Assert.Throws<ArgumentException>(() => new Lesmoment(new DateTime(1900, 1, 1), new DateTime(1900, 2, 2), _context.Lijst1Gebruiker));
         }
         #endregion
 
         #region registreerLidTests
-        /*
+
         [Fact]
         public void RegistreerLid_OnbestaandeGebruiker_valid()
         {
             lesmoment.RegistreerLid(_context.GebruikerNietInLijst);
             Assert.Contains(_context.GebruikerNietInLijst, lesmoment.geefAanwezigeLeden());
         }
-        */
 
-        /*
         [Fact]
         public void RegistreerLid_IngeschrevenGebruiker_Valid()
         {
             lesmoment.RegistreerLid(_context.GebruikerInLijst);
             Assert.Contains(_context.GebruikerInLijst, lesmoment.geefAanwezigeLeden());
         }
-        */
+
         #endregion
     }
 }
