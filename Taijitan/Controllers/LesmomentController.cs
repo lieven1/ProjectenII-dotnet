971088cb6c3883cc6dcecf63b45c1e4cf7fd6606
Steve Kusters <steve.kusters@hotmail.com>
2019-03-09 18:37:04 +0100

    Index methode Lesmomentcontroller geïmplementeerd (lijst met lesmomenten in db wordt getoond)

---
+++ Taijitan/Controllers/LesmomentController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Taijitan.Models.Domain;
+
+namespace Taijitan.Controllers
+{
+    public class LesmomentController : Controller
+    {
+        private ILesmomentRepository lesmomentRepository;
+
+        public LesmomentController(ILesmomentRepository lesmomentRepository)
+        {
+            this.lesmomentRepository = lesmomentRepository;
+        }
+
+        public IActionResult Index()
+        {
+            List<Lesmoment> lesmomenten = lesmomentRepository.GetAll();
+
+            return View(lesmomenten.OrderBy(l => l.Datum));
+        }
+
+        public IActionResult Get()
+        {
+
+        }
+    }
+}
