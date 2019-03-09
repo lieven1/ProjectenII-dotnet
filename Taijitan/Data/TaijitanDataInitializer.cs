687cfe78b02d33da46bed28c245cc96e9de19d17
Steve Kusters <steve.kusters@hotmail.com>
2019-03-09 18:36:19 +0100

    maak lesmomenten aan in db bij opstart

--- Taijitan/Data/TaijitanDataInitializer.cs
+++ Taijitan/Data/TaijitanDataInitializer.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
+using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Taijitan.Models.Domain;
+using Taijitan.Models.Domain.Databindings;
 using Taijitan.Models.Domain.Enums;
 
 namespace Taijitan.Data
@@ -28,12 +30,20 @@ namespace Taijitan.Data
                 _context.Adressen.Add(adres1);
                 _context.Adressen.Add(adres2);
 
-                Gebruiker BruceLee = new Gebruiker("taijitan2", "11111111111", new DateTime(2018, 05, 16),"Lee", "Bruce", Geslacht.Man, new DateTime(1940, 11, 27), "UZ Gent", null, "0479076258", "BruceLee@MartialArt.com", "BruceLeesMom@MartialArt.com", adres1, 100, new Gradatie(1, "Kyo", "Sho-kyo"), TypeGebruiker.Lid);
+                Gebruiker BruceLee = new Gebruiker("taijitan2", "11111111111", new DateTime(2018, 05, 16), "Lee", "Bruce", Geslacht.Man, new DateTime(1940, 11, 27), "UZ Gent", null, "0479076258", "BruceLee@MartialArt.com", "BruceLeesMom@MartialArt.com", adres1, 100, new Gradatie(1, "Kyo", "Sho-kyo"), TypeGebruiker.Lid);
                 Gebruiker Lid = new Gebruiker("lid2", "12312312312", new DateTime(2018, 05, 24), "John", "Doe", Geslacht.Man, new DateTime(1960, 3, 24), "Brussel", "0525252525", "0479076258", "lid@MartialArt.com", "LidsMom@MartialArt.com", adres1, 100, new Gradatie(1, "Kyo", "Sho-Dan"), TypeGebruiker.Lid);
                 _context.Gebruikers.Add(BruceLee);
                 _context.Gebruikers.Add(Lid);
                 _context.SaveChanges();
 
+                LesmomentLeden lesmomentLedenBruceLee = new LesmomentLeden(BruceLee);
+                LesmomentLeden lesmomentLedenLid = new LesmomentLeden(Lid);
+                List<LesmomentLeden> lesmomentLeden = new List<LesmomentLeden>();
+                lesmomentLeden.Add(lesmomentLedenBruceLee);
+                lesmomentLeden.Add(lesmomentLedenLid);
+                Lesmoment lesmoment1 = new Lesmoment(DateTime.Now, new DateTime(2019, 3, 16, 14, 0, 0), new DateTime(2019, 3, 16, 18, 0, 0), lesmomentLeden);
+
+                _context.Lesmomenten.Add(lesmoment1);
                 await InitializeUsers();
             }
         }
