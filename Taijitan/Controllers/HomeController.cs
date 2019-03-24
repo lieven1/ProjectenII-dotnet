fffec24a69d0413bd9e05896b18627f03bef957d
Bono Opalfvens <bono-opalfvens@hotmail.com>
2019-03-24 19:25:11 +0100

    [Feat] GebruikersDashboard aanpassen

--- Taijitan/Controllers/HomeController.cs
+++ Taijitan/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -8,15 +9,15 @@ using Taijitan.Models.Domain;
 
 namespace Taijitan.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
-        [Authorize]
         [ServiceFilter(typeof(AspUserToGebruiker))]
         public IActionResult Index(Gebruiker gebruiker) {
             if (User.IsInRole("gebruiker"))
             {
                 HttpContext.Session.SetString("Gebruiker", gebruiker.Gebruikersnaam);
-                return RedirectToAction("leeromgeving", "gebruiker");
+                return RedirectToAction(nameof(Dashboard));
             }
             else if(User.IsInRole("beheerder"))
             {
@@ -25,8 +26,20 @@ namespace Taijitan.Controllers
             return RedirectToAction(nameof(Error));
         }
 
-        
+        public IActionResult Login(String gebruikersnaam)
+        {
+            HttpContext.Session.SetString("Gebruiker", gebruikersnaam);
+            return RedirectToAction(nameof(Dashboard));
+        }
 
+        public IActionResult Dashboard()
+        {
+            TempData["leeromgeving"] = User.IsInRole("beheerder");
+            TempData["gebruiker"] = HttpContext.Session.GetString("Gebruiker");
+            return View();
+        }
+
+        [AllowAnonymous]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
