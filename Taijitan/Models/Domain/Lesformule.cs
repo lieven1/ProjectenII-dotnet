6922fd8a1cb8cff9708ade169e040929a97b015a
stevek19 <steve.kusters@student.hogent.be>
2019-03-25 17:13:13 +0100

    lesformule als volwaardige klasse toegevoegd ipv enum

---
+++ Taijitan/Models/Domain/Lesformule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace Taijitan.Models.Domain
+{        
+    public class Lesformule
+    {
+        public int Id { get; private set; }
+        [NotMapped]
+        public List<DayOfWeek> Days { get; private set; }
+        public string TitleText { get; private set; }
+        public string LesText { get; private set; }
+
+        public Lesformule(List<DayOfWeek> days, string titleText, string lesText)
+        {
+            Days = days;
+            TitleText = titleText;
+            LesText = lesText;
+        }
+
+        public Lesformule(string titleText, string lesText) : this(new List<DayOfWeek>(), titleText, lesText) { }
+
+        public Lesformule()
+        {
+
+        }
+
+    }
+    
+}
